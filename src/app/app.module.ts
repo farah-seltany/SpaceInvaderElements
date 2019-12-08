7257d8244455623ea9cd260eb9481eb853580f89
Farah SELTANY <farahseltany@gmail.com>
2019-12-08 22:16:16 +0100

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AmmoComponent } from './ammo/ammo.component';
+import { BonusComponent } from './bonus/bonus.component';
+import { BossComponent } from './boss/boss.component';
+import { BossAmmoComponent } from './boss-ammo/boss-ammo.component';
+import { EnemyComponent } from './enemy/enemy.component';
+import { GameOverComponent } from './game-over/game-over.component';
+import { ObstacleComponent } from './obstacle/obstacle.component';
+import { PauseComponent } from './pause/pause.component';
+import { ShipComponent } from './ship/ship.component';
+import { VictoryComponent } from './victory/victory.component';
+import { GameComponent } from './game/game.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { OptionsComponent } from './options/options.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AmmoComponent,
+    BonusComponent,
+    BossComponent,
+    BossAmmoComponent,
+    EnemyComponent,
+    GameOverComponent,
+    ObstacleComponent,
+    PauseComponent,
+    ShipComponent,
+    VictoryComponent,
+    GameComponent,
+    HomepageComponent,
+    OptionsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
